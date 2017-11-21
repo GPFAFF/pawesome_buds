b678becf39e5c8d28a749c58ea52b9f29d8da150
Greg Pfaff <greg.pfaff@razorfish.com>
2017-11-21 12:27:39 -0600

    adding markup updates to templates

--- src/components/container/AboutPage.js
+++ src/components/container/AboutPage.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 
 class About extends Component {
   render() {
     return (
       <div className="about">
-        <h1>About</h1>
+        <h2>About</h2>
           <p>about Lorem ipsum Lorem ipsum dolor sit amet,
              consectetur adipisicing elit. Harum, consectetur, at obcaecati accusantium facilis aut voluptates unde accusamus eos.
              Error quo aut vitae et dolorum, ratione necessitatibus
              tempora omnis obcaecati?</p>
-          {/* <Link to="/home" className="button">Home </Link> */}
+          <Link to="/home" className="button">Home </Link>
       </div>
     );
   }
--- src/components/container/HomePage.js
+++ src/components/container/HomePage.js
@@ -4,18 +4,15 @@ import { Link } from 'react-router-dom';
 class HomePage extends Component {
   render() {
     return (
-      // <div className="home">
-      //   <h1> Pawesome Buds </h1>
-      //   <p>Home Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores repellendus aperiam amet, doloribus itaque sit molestias       voluptatum autem sint eaque numquam omnis et. Odio sed labore enim dolorum ullam possimus.</p>
-      // </div>
       <div className="home">
-          <p>Home Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit. Dolores repellendus
-            aperiam amet, doloribus itaque sit molestias
-             voluptatum autem sint eaque numquam omnis
-             et. Odio sed labore enim dolorum ullam
-             possimus.</p>
-          <Link to="/about" className="button">Learn More </Link>
+        <h2>DOGES</h2>
+        <p>Home Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Dolores repellendus
+          aperiam amet, doloribus itaque sit molestias
+            voluptatum autem sint eaque numquam omnis
+            et. Odio sed labore enim dolorum ullam
+            possimus.</p>
+        <Link to="/about" className="button">Learn More </Link>
       </div>
     );
   }
