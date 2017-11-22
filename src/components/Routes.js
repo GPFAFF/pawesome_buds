76e6de4f01b1ce6c0b0d22680c6ee2621f95964b
Greg Pfaff <greg.pfaff@razorfish.com>
2017-11-22 08:27:34 -0600

    massive cleanup changes routes adds styling

---
+++ src/components/Routes.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import HomePage from './container/HomePage';
+import FormPage from './container/FormPage';
+import AboutPage from './container/AboutPage';
+import NotFound from './container/NotFound';
+
+class Routes extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/home" exact component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/form" component={FormPage} />
+        <Route component={NotFound} />
+      </Switch>
+    );
+  }
+}
+
+export default Routes;
